c380d81dd9096564614434d2d3440329835ed1e8
KVIKERITAS <aicamaurisk@gmail.com>
2024-01-13 20:09:09 +0200

    Working registration functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,5 +1,8 @@
 'use server'
+import { getUserByEmail } from '@/data/user'
+import { db } from '@/lib/db'
 import { RegisterSchema } from '@/schemas'
+import bcrypt from 'bcrypt'
 import * as z from 'zod'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -7,5 +10,14 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
 	if (!validatedFields.success) return { error: 'Invalid credentials' }
 
-	return { success: 'Email sent!' }
+	const { email, password, username } = validatedFields.data
+	const hash = await bcrypt.hash(password, 10)
+
+	const existingUser = await getUserByEmail(email)
+
+	if (existingUser) return { error: 'Email already in use!' }
+
+	await db.user.create({ data: { username, email, password: hash } })
+
+	return { success: 'User created!' }
 }
