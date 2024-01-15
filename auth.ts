74e7f4bb1e121eaed099942f449f35747f87dbdf
KVIKERITAS <aicamaurisk@gmail.com>
2024-01-15 18:19:03 +0200

    Made it possible to login in only as ADMIN using callbacks

--- auth.ts
+++ auth.ts
@@ -1,9 +1,22 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 
+import { getUserById } from '@/data/user'
 import { db } from '@/lib/db'
 
 import authConfig from '@/auth.config'
+
+type TUserRole = 'ADMIN' | 'USER'
+
+declare module 'next-auth' {
+	interface Session {
+		user: {
+			id: string
+			role: 'ADMIN' | 'USER'
+			username: string
+		}
+	}
+}
 
 export const {
 	handlers: { GET, POST },
@@ -11,6 +24,41 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	callbacks: {
+		async signIn({ user }) {
+			const existingUser = await getUserById(user.id)
+
+			if (!existingUser || existingUser.role === 'USER') {
+				return false
+			}
+
+			return true
+		},
+
+		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub
+			}
+
+			if (token.role && session.user) {
+				session.user.role = token.role as TUserRole
+				session.user.username = token.username as string
+			}
+
+			return session
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token
+
+			const existingUser = await getUserById(token.sub)
+			if (!existingUser) return token
+
+			token.username = existingUser.username
+			token.role = existingUser.role
+
+			return token
+		},
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: 'jwt' },
 	...authConfig,
