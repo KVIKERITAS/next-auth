bddb9827cdac4eb0e66ee4597670b894fce0880c
KVIKERITAS <aicamaurisk@gmail.com>
2024-01-16 20:16:15 +0200

    Implemented Google and GitHub OAuths

--- auth.ts
+++ auth.ts
@@ -24,13 +24,25 @@ export const {
 	signIn,
 	signOut,
 } = NextAuth({
+	pages: {
+		signIn: '/auth/login',
+		error: '/auth/error',
+	},
+	events: {
+		async linkAccount({ user }) {
+			await db.user.update({
+				where: { id: user.id },
+				data: { emailVerified: new Date() },
+			})
+		},
+	},
 	callbacks: {
 		async signIn({ user }) {
-			const existingUser = await getUserById(user.id)
+			// const existingUser = await getUserById(user.id)
 
-			if (!existingUser || existingUser.role === 'USER') {
-				return false
-			}
+			// if (!existingUser || existingUser.role === 'USER') {
+			// 	return false
+			// }
 
 			return true
 		},
